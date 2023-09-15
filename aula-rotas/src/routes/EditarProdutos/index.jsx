e5bf3b60d339b636972ab9a30b0272deb51e7d43
Pedro Gomes <pedro.gomesf2004@gmail.com>
2023-09-15 01:46:42 -0300

    modificado EditarProduto.

--- aula-rotas/src/routes/EditarProdutos/index.jsx
+++ aula-rotas/src/routes/EditarProdutos/index.jsx
@@ -1,8 +1,18 @@
+import { Link } from 'react-router-dom'
+import { listaProdutos } from '../../components/ListaProdtos'
+
 export default function EditarProduto(){
 
     return(
         <main>
             <h1>Editando o produto</h1>
+            {listaProdutos.map(prod => (
+                <div key={prod.id}>
+                    <Link to={`/produtos/editar/${prod.id}`}>
+                        Editar o produto: {prod.nome}
+                    </Link>
+                </div>
+            ))}
         </main>
     )
 }
