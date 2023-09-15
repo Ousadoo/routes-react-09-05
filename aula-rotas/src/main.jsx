5480de9812233b11ca5180c575a102b2e80dfb85
Pedro Gomes <pedro.gomesf2004@gmail.com>
2023-09-15 01:32:56 -0300

    Feito Main.

--- aula-rotas/src/main.jsx
+++ aula-rotas/src/main.jsx
@@ -8,6 +8,28 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App/>,
+    errorElement: <Error/>,
+    children: [
+      {
+        path: "/",
+        element: <Home/>
+      },
+      {
+        path: "/produtos",
+        element: <Produtos/>
+      },
+      {
+        path: "/produtos/editar/:id",
+        element: <EditarProdutos/>
+      },
+    ]
+  }
+])
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
